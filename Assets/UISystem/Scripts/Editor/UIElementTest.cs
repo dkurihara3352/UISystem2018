ef65f641f8f2ecca206a9dac2cb44ddcaf6b7bc2
Daisuke Kurihara <dkurihara3352@gmail.com>
2018-05-26 15:26:26 +0900

    workspace cleaned up

---
+++ Assets/UISystem/Scripts/Editor/UIElementTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using NSubstitute;
+using UISystem;
+
+public class UIElementTest {
+
+	[Test]
+	public void UIElement_Instantiation(){
+		IUIElement uie = new UIElement();
+		Assert.That(uie, Is.Not.Null);
+	}
+}
