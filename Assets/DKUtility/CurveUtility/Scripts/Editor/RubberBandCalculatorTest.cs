06777f84d9576226e60f239c155725b4446426a2
Daisuke Kurihara <dkurihara3352@gmail.com>
2018-07-15 23:03:07 +0900

    rubber band ok

---
+++ Assets/DKUtility/CurveUtility/Scripts/Editor/RubberBandCalculatorTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using NSubstitute;
+using DKUtility.CurveUtility;
+using DKUtility;
+
+[TestFixture]
+public class RubberBandCalculatorTest {
+	// [Test, TestCaseSource(typeof(CalcRubberBandValue_Demo_TestCase), "cases")]
+	public void CalcRubberBandValue_Demo(float suppleness, float limitLength){
+		IRubberBandCalculator calculator = new RubberBandCalculator(suppleness, limitLength);
+		float origDisp = 0f;
+		int stepSize = Mathf.RoundToInt(limitLength);
+		float prev = 0f;
+		float diffThreshold = 0.5f;
+		float rubberBandedValue = 0f;
+		int iteration = 0;
+		DebugHelper.PrintInRed("suppleness: " + suppleness.ToString() + ", limitLength: " + limitLength.ToString());
+		while(rubberBandedValue + diffThreshold < limitLength){
+			rubberBandedValue = calculator.CalcRubberBandValue(origDisp);
+			float delta = rubberBandedValue - prev;
+			Debug.Log("iteration: " + iteration.ToString() + ", origDisp: " + origDisp.ToString() + ", rubberValue: " + rubberBandedValue.ToString() + ", delta: " + delta.ToString());
+			prev = rubberBandedValue;
+			origDisp += stepSize;
+			iteration ++;
+		}
+	}
+	public class CalcRubberBandValue_Demo_TestCase{
+		public static object[] cases = {
+			new object[]{1f, 10f},
+			new object[]{1f, 5f},
+			new object[]{10f, 10f},
+			new object[]{.5f, 10f},
+		};
+	}
+}
---
+++ Assets/DKUtility/CurveUtility/Scripts/RubberBandCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DKUtility.CurveUtility{
+	public interface IRubberBandCalculator{
+		float CalcRubberBandValue(float originalDisplacement);
+	}
+	public class RubberBandCalculator: IRubberBandCalculator{
+		public RubberBandCalculator(float suppleness, float limitLength){
+			suppleness = GetSupplenessInRrange(suppleness);
+			thisSuppleness = suppleness;
+			thisLimitLength = limitLength;
+		}
+		readonly float thisSuppleness;
+		readonly float thisLimitLength;
+		float GetSupplenessInRrange(float original){
+			/*  if original exceeds 1f, rubberBand value exceeds original displacement
+			*/
+			if(original > 1f)
+				return 1f;
+			else
+				return original;
+		}
+		public float CalcRubberBandValue(float originalDisplacement){
+			float denominator = thisLimitLength + (thisSuppleness * originalDisplacement);
+			float numerator = originalDisplacement * thisLimitLength * thisSuppleness;
+			if(denominator != 0f && numerator != 0f)
+				return numerator/ denominator;
+			else
+				return 0f;
+		}
+	}
+}
