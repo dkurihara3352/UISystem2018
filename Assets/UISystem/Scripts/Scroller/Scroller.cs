eb67e155b93b16de0d34ea72d8a5f34a55ab885b
Daisuke Kurihara <dkurihara3352@gmail.com>
2018-07-16 08:43:30 +0900

    implementScroller

---
+++ Assets/UISystem/Scripts/Scroller/Scroller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UISystem{
+	public interface IScroller: IUIElement{}
+	public class Scroller : AbsUIElement, IScroller{
+
+	}
+}
