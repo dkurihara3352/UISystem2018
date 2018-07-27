db535727ad2aa39587c9bdf914aecf3e786aebac
Daisuke Kurihara <dkurihara3352@gmail.com>
2018-07-27 18:17:57 +0900

    inertialScroll process done

--- Assets/UISystem/Scripts/Scroller/ScrollerElementMotorProcess/Editor/InertialScrollProcessTest.cs
+++ Assets/UISystem/Scripts/Scroller/ScrollerElementMotorProcess/Editor/InertialScrollProcessTest.cs
@@ -9,5 +9,102 @@ using DKUtility;
 
 [TestFixture, Category("UISystem")]
 public class InertialScrollProcessTest {
+    [Test, TestCaseSource(typeof(Construction_DecelerationNotGreaterThanZero_TestCase), "cases")]
+    public void Construction_DecelerationNotGreaterThanZero_ThrowsException(float deceleration){
+        ITestInertialScrollProcessConstArg arg = CreateMockConstArg();
+        arg.processManager.GetInertialScrollDeceleration().Returns(deceleration);
+        
+        Assert.Throws(
+            Is.TypeOf(typeof(System.InvalidOperationException)).And.Message.EqualTo("deceleration must be greater than zero"),
+            () => {
+                new TestInertialScrollProcess(arg);
+            }
+        );
+    }
+    public class Construction_DecelerationNotGreaterThanZero_TestCase{
+        public static object[] cases = {
+            new object[]{0f},
+            new object[]{-1f},
+        };
+    }
+    [Test, TestCaseSource(typeof(Construction_SetsExpireTime_TestCase), "cases")]
+    public void Construction_SetsExpireTime(float initialDeltaPosOnAxis, float deceleration, float expected){
+        ITestInertialScrollProcessConstArg arg = CreateMockConstArg();
+        arg.processManager.GetInertialScrollDeceleration().Returns(deceleration);
+        arg.initialDeltaPosOnAxis.Returns(initialDeltaPosOnAxis);
+        TestInertialScrollProcess process = new TestInertialScrollProcess(arg);
 
-}
+        float actual = process.thisExpireTime_Test;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    public class Construction_SetsExpireTime_TestCase{
+        public static object[] cases = {
+            new object[]{12f, 3f, 4f},
+            new object[]{-12f, 3f, 4f},
+        };
+    }
+    [Test, TestCaseSource(typeof(UpadateProcess_Demo_TestCase), "cases"), Ignore]
+    public void UpadateProcess_Demo(float initDeltaPosOnAxis, float deceleration, int dimension, Vector2 elementInitLocalPos){
+        ITestInertialScrollProcessConstArg arg = CreateMockConstArg();
+        arg.processManager.GetInertialScrollDeceleration().Returns(deceleration);
+        arg.initialDeltaPosOnAxis.Returns(initDeltaPosOnAxis);
+        arg.dimension.Returns(dimension);
+        IUIElement scrollerElement = Substitute.For<IUIElement>();
+        scrollerElement.GetLocalPosition().Returns(elementInitLocalPos);
+        arg.scrollerElement.Returns(scrollerElement);
+        TestInertialScrollProcess process = new TestInertialScrollProcess(arg);
+
+        bool done = false;
+        DebugHelper.PrintInRed("initVel: " + initDeltaPosOnAxis.ToString() + ", deceleration: " + deceleration.ToString());
+        float deltaT = .1f;
+        Vector2 localPos = elementInitLocalPos;
+        scrollerElement.SetLocalPosition(Arg.Do<Vector2>(x => localPos = x));
+        while(!done){
+            process.UpdateProcess(deltaT);
+            
+            Debug.Log("elementLocalPos: " + localPos.ToString() + ", velocity: " + process.thisPrevVelocity_Test.ToString());
+            if(process.thisElapsedTime_Test >= process.thisExpireTime_Test)
+                done = true;
+        }
+    }
+    public class UpadateProcess_Demo_TestCase{
+        public static object[] cases = {
+            new object[]{10f, 10f, 0, new Vector2(15f, 15f)},
+            new object[]{-10f, 10f, 0, new Vector2(15f, 15f)},
+        };
+    }
+
+
+
+
+
+
+
+    public class TestInertialScrollProcess: InertialScrollProcess{
+        public TestInertialScrollProcess(ITestInertialScrollProcessConstArg arg): base(arg.initialDeltaPosOnAxis, arg.scroller, arg.scrollerElement, arg.dimension, arg.processManager){}
+        public float thisDeceleration_Test{get{return thisDeceleration;}}
+        public float thisExpireTime_Test{get{return thisExpireTime;}}
+        public float thisElapsedTime_Test{get{return thisElapsedTime;}}
+        public float thisPrevVelocity_Test{get{return thisPrevVelocity;}}
+    }
+    public interface ITestInertialScrollProcessConstArg{
+        float initialDeltaPosOnAxis{get;}
+        IScroller scroller{get;}
+        IUIElement scrollerElement{get;}
+        int dimension{get;}
+        IProcessManager processManager{get;}
+    }
+    public ITestInertialScrollProcessConstArg CreateMockConstArg(){
+        ITestInertialScrollProcessConstArg arg = Substitute.For<ITestInertialScrollProcessConstArg>();
+        arg.initialDeltaPosOnAxis.Returns(0f);
+        arg.scroller.Returns(Substitute.For<IScroller>());
+        arg.scrollerElement.Returns(Substitute.For<IUIElement>());
+        arg.dimension.Returns(0);
+        IProcessManager processManager = Substitute.For<IProcessManager>();
+        processManager.GetInertialScrollDeceleration().Returns(1f);
+        arg.processManager.Returns(processManager);
+
+        return arg;
+    }
+}   
--- Assets/UISystem/Scripts/Scroller/ScrollerElementMotorProcess/InertialScrollProcess.cs
+++ Assets/UISystem/Scripts/Scroller/ScrollerElementMotorProcess/InertialScrollProcess.cs
@@ -6,36 +6,41 @@ using DKUtility;
 namespace UISystem{
 	public interface IInertialScrollProcess: IScrollerElementMotorProcess{}
 	public class InertialScrollProcess: AbsScrollerElementMotorProcess, IInertialScrollProcess{
-		public InertialScrollProcess(float deltaPosOnAxis, IScroller scroller, IUIElement scrollerElement, int dimension, IProcessManager processManager): base(scroller, scrollerElement, dimension, processManager){
-			thisInitialVelocity = deltaPosOnAxis;
+		public InertialScrollProcess(float initialDeltaPosOnAxis, IScroller scroller, IUIElement scrollerElement, int dimension, IProcessManager processManager): base(scroller, scrollerElement, dimension, processManager){
+			thisInitialVelocity = initialDeltaPosOnAxis;
+			thisPrevVelocity = thisInitialVelocity;
+			thisPrevLocalPosOnAxis = scrollerElement.GetLocalPosition()[dimension];
 			float deceleration = processManager.GetInertialScrollDeceleration();
 			thisDeceleration = MakeSureDecelerationIsGreaterThanZero(deceleration);
 
 			thisExpireTime = Mathf.Abs(thisInitialVelocity / thisDeceleration);
 		}
 		readonly float thisInitialVelocity;
-		readonly float thisDeceleration;
+		readonly protected float thisDeceleration;
 		float MakeSureDecelerationIsGreaterThanZero(float source){
-			if(source < 0f)
+			if(source <= 0f)
 				throw new System.InvalidOperationException("deceleration must be greater than zero");
 			return source;
 		}
-		readonly float thisStopThresholdDeltaVelocity;
-		readonly float thisExpireTime;
-		bool thisIsScrolledInPositiveDirection{
-			get{return thisInitialVelocity > 0f;}
-		}
+		readonly protected float thisExpireTime;
 
-		float thisPrevVelocity = 0f;
-		float thisElapsedTime = 0f;
+		protected float thisPrevVelocity;
+		protected float thisElapsedTime = 0f;
+		float thisPrevLocalPosOnAxis;
 		public override void UpdateProcess(float deltaT){
 			thisElapsedTime += deltaT;
 
-			float deltaV = - (deltaT * thisDeceleration);
+			float deltaV = - deltaT * thisDeceleration;
+			if(thisInitialVelocity < 0f)
+				deltaV *= -1f;
+
 			float newVelocity = thisPrevVelocity + deltaV;
 			float deltaPosOnAxis = newVelocity * deltaT;
-			UpdateScrollerElementLocalPosOnAxis(deltaPosOnAxis);
+			float newLocalPosOnAxis = thisPrevLocalPosOnAxis + deltaPosOnAxis;
+			// UpdateScrollerElementLocalPosOnAxis(deltaPosOnAxis);
+			SetScrollerElementLocalPosOnAxis(newLocalPosOnAxis);
 			thisPrevVelocity = newVelocity;
+			thisPrevLocalPosOnAxis = newLocalPosOnAxis;
 			thisScroller.CheckForDynamicBoundarySnap(deltaPosOnAxis, thisDimension);
 
 			if(thisElapsedTime >= thisExpireTime)
